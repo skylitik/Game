1b10453e92aa40860d0218f65621e76e569a9518
Sergii Ostraczerta <skylitik@outlook.com>
2020-12-14 18:28:14 +0100

    Game TicTacToe

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Game;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
+            Engine ticTacToe = new Engine();
+            ticTacToe.PlayGame();
+            Console.WriteLine("Game over");
         }
     }
 }
